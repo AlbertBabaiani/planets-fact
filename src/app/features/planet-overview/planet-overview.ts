eb02ef15904209ccdd60d355f18fa65cee17183d
Albert Babaiani <babaianialbert@gmail.com>
2026-03-05 23:38:51 +0400

    add surface view

--- src/app/features/planet-overview/planet-overview.ts
+++ src/app/features/planet-overview/planet-overview.ts
@@ -1,4 +1,4 @@
-import { Component, computed, inject, input } from '@angular/core';
+import { Component, computed, inject, input, signal } from '@angular/core';
 import { Planets } from '../../core/planets';
 import { RouterLink } from '@angular/router';
 
@@ -13,6 +13,8 @@ export class PlanetOverview {
 
   planetName = input.required<string>();
 
+  section = signal<'overview' | 'structure' | 'geology'>('overview');
+
   planet = computed(() => {
     return this.service
       .planetsData()
@@ -20,4 +22,35 @@ export class PlanetOverview {
 
     // Add Empty Error
   });
+
+  // Helpers Start
+
+  getMainImg(): string {
+    let link = `assets/images/planets/${this.planetName()}/`;
+
+    switch (this.section()) {
+      case 'structure':
+        return link + `planet-${this.planetName()}-internal.svg`;
+      default:
+        return link + `planet-${this.planetName()}.svg`;
+    }
+  }
+
+  getSurfaceImg(): string {
+    return `assets/images/planets/${this.planetName().toLowerCase()}/geology-${this.planetName().toLowerCase()}.png`;
+  }
+
+  getAltText(): string {
+    return `Image of ${this.planetName()[0].toUpperCase() + this.planetName().slice(1).toLowerCase()} ${this.section()}.`;
+  }
+
+  getTextContent(): string {
+    return this.planet()?.[this.section()]?.content;
+  }
+
+  getWikipediaLink(): string {
+    return this.planet()?.[this.section()]?.source;
+  }
+
+  // Helpers End
 }
